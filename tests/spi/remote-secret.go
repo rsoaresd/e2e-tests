b50c98a55a6a34806a74574becc811293cdb9cef
Rafaela Soares <rsoaresd@redhat.com>
2023-08-11 16:26:01 +0100

    SVPI-558

--- tests/spi/remote-secret.go
+++ tests/spi/remote-secret.go
@@ -17,27 +17,28 @@ import (
 
 /*
  * Component: spi
- * Description: SVPI-541 - Basic remote secret functionalities
+ * Description: SVPI-558 - Test all the options of the authz of remote secret target deployment
  */
 
 var _ = framework.SPISuiteDescribe(Label("spi-suite", "remote-secret"), func() {
 
 	defer GinkgoRecover()
 
-	var fw *framework.Framework
-	var err error
-	var namespace string
-	var remoteSecret *v1beta1.RemoteSecret
-	targetNamespace1 := fmt.Sprintf("spi-test-target1-%s", util.GenerateRandomString(4))
-	targetNamespace2 := fmt.Sprintf("spi-test-target2-%s", util.GenerateRandomString(4))
-	remoteSecretName := "test-remote-secret"
-	targetSecretName1 := ""
-	targetSecretName2 := ""
-	serviceAccountName := fmt.Sprintf("deployment-enabler-%s", util.GenerateRandomString(4))
-	roleName := fmt.Sprintf("deployment-enabler-%s", util.GenerateRandomString(4))
-	roleBindingName := fmt.Sprintf("deployment-enabler-%s", util.GenerateRandomString(4))
+	Describe("SVPI-558 - Using service account to authenticate and deploy to the target namespaces", Ordered, func() {
+		var fw *framework.Framework
+		var err error
+		var namespace string
+		var remoteSecret *v1beta1.RemoteSecret
 
-	Describe("SVPI-541 - Basic remote secret functionalities", Ordered, func() {
+		remoteSecretName := fmt.Sprintf("test-remote-secret-%s", util.GenerateRandomString(4))
+		targetNamespace1 := fmt.Sprintf("spi-test-target1-%s", util.GenerateRandomString(4))
+		targetNamespace2 := fmt.Sprintf("spi-test-target2-%s", util.GenerateRandomString(4))
+		targetSecretName1 := ""
+		targetSecretName2 := ""
+		serviceAccountName := fmt.Sprintf("deployment-enabler-%s", util.GenerateRandomString(4))
+		roleName := fmt.Sprintf("deployment-enabler-%s", util.GenerateRandomString(4))
+		roleBindingName := fmt.Sprintf("deployment-enabler-%s", util.GenerateRandomString(4))
+
 		BeforeAll(func() {
 			// Initialize the tests controllers
 			fw, err = framework.NewFramework(utils.GetGeneratedNamespace("spi-demos"))
@@ -172,6 +173,89 @@ var _ = framework.SPISuiteDescribe(Label("spi-suite", "remote-secret"), func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			_, err = fw.AsKubeAdmin.CommonController.GetSecret(targetNamespace2, targetSecretName2)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("SVPI-558 - Target to the same namespace where the remote secret lives is always deployed", Ordered, func() {
+		var fw *framework.Framework
+		var err error
+		var namespace string
+		var remoteSecret *v1beta1.RemoteSecret
+		remoteSecretName := fmt.Sprintf("test-remote-secret-%s", util.GenerateRandomString(4))
+		targetSecretName := ""
+
+		BeforeAll(func() {
+			// Initialize the tests controllers
+			fw, err = framework.NewFramework(utils.GetGeneratedNamespace("spi-demos"))
+			Expect(err).NotTo(HaveOccurred())
+			namespace = fw.UserNamespace
+			Expect(namespace).NotTo(BeEmpty())
+		})
+
+		AfterAll(func() {
+			if !CurrentSpecReport().Failed() {
+				Expect(fw.SandboxController.DeleteUserSignup(fw.UserName)).To(BeTrue())
+			}
+		})
+
+		It("creates RemoteSecret with a target that shares the same namespace", func() {
+			remoteSecret, err = fw.AsKubeDeveloper.SPIController.CreateRemoteSecret(remoteSecretName, namespace, []string{namespace})
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() bool {
+				remoteSecret, err = fw.AsKubeDeveloper.SPIController.GetRemoteSecret(remoteSecretName, namespace)
+				Expect(err).NotTo(HaveOccurred())
+
+				return meta.IsStatusConditionFalse(remoteSecret.Status.Conditions, "DataObtained")
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), fmt.Sprintf("RemoteSecret %s/%s is not waiting for data", namespace, remoteSecretName))
+		})
+
+		It("creates upload secret", func() {
+			s := &v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-secret",
+					Labels: map[string]string{
+						"appstudio.redhat.com/upload-secret": "remotesecret",
+					},
+					Annotations: map[string]string{
+						"appstudio.redhat.com/remotesecret-name": remoteSecret.Name,
+					},
+				},
+				Type: v1.SecretTypeOpaque,
+				StringData: map[string]string{
+					"a": "b",
+					"c": "d",
+				},
+			}
+
+			_, err = fw.AsKubeAdmin.CommonController.CreateSecret(namespace, s)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("checks if remote secret was deployed", func() {
+			Eventually(func() bool {
+				remoteSecret, err = fw.AsKubeDeveloper.SPIController.GetRemoteSecret(remoteSecretName, namespace)
+				Expect(err).NotTo(HaveOccurred())
+
+				return meta.IsStatusConditionTrue(remoteSecret.Status.Conditions, "Deployed")
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), fmt.Sprintf("RemoteSecret %s/%s is not in deployed phase", namespace, remoteSecretName))
+		})
+
+		It("checks targets in RemoteSecret status", func() {
+			remoteSecret, err = fw.AsKubeDeveloper.SPIController.GetRemoteSecret(remoteSecret.Name, namespace)
+			Expect(err).NotTo(HaveOccurred())
+
+			targets := remoteSecret.Status.Targets
+			Expect(targets).To(HaveLen(1))
+
+			// get targetSecretName
+			targetSecretName = fw.AsKubeDeveloper.SPIController.GetTargetSecretName(targets, namespace)
+			Expect(targetSecretName).ToNot(BeEmpty())
+		})
+
+		It("checks if secret was created in target namespace", func() {
+			_, err = fw.AsKubeAdmin.CommonController.GetSecret(namespace, targetSecretName)
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})
